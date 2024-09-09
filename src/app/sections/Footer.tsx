c12759494a36f8c492abef7e614a31681f926c5e
Adam Strojil <adam.strojil@gmail.com>
2024-09-09 23:54:21 +0200

    fix footer align on mobile

--- src/app/sections/Footer.tsx
+++ src/app/sections/Footer.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Link } from '../components/Link';
 
+const NO_BREAK_SPACE = '\xa0'; //TODO refactor with css
+
 export const Footer = () => (
-    <footer className="text-right mt-2 text-gray-700">
-        Design inspired by <Link href="https://sanatrath.com/">Sanat Rath</Link>.
+    <footer className="text-right mt-2 text-gray-700 flex justify-center">
+        Design{NO_BREAK_SPACE}inspired{NO_BREAK_SPACE}by{NO_BREAK_SPACE}
+        <Link href="https://sanatrath.com/">Sanat{NO_BREAK_SPACE}Rath</Link>.
     </footer>
 );
