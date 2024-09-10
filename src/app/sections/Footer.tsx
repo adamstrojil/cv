1d70fd2788586edda32b3e6eab5dbd101ad0dcf7
Adam Strojil <adam.strojil@gmail.com>
2024-09-10 10:07:06 +0200

    revert footer center align

--- src/app/sections/Footer.tsx
+++ src/app/sections/Footer.tsx
@@ -4,7 +4,7 @@ import { Link } from '../components/Link';
 const NO_BREAK_SPACE = '\xa0'; //TODO refactor with css
 
 export const Footer = () => (
-    <footer className="text-right mt-2 text-gray-700 flex justify-center">
+    <footer className="text-right mt-2 text-gray-700">
         Design{NO_BREAK_SPACE}inspired{NO_BREAK_SPACE}by{NO_BREAK_SPACE}
         <Link href="https://sanatrath.com/">Sanat{NO_BREAK_SPACE}Rath</Link>.
     </footer>
